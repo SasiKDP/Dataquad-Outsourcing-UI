fec5db39278c8d9ad2a6bf8b2c1861a35073f6f7
SasiKDP <sasank9110@gmail.com>
2025-06-04 11:18:40 +0530

    changd the ip addresss

--- src/Services/httpService.js
+++ src/Services/httpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // DEV
-const DEV_API_BASE_URL = 'http://182.18.177.16'; // Replace with your actual
+const DEV_API_BASE_URL = 'http://192.168.0.246'; // Replace with your actual
 export const API_BASE_URL = DEV_API_BASE_URL
 
 // PROD
